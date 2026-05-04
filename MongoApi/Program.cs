b73cea48976301c7baeda9e7b6f6b1504041c030
noahbachmann <trainpaths@gmail.com>
2026-05-04 11:48:25 +0200

    feat: improve dockerfile and api to work with docker compose

--- MongoApi/Program.cs
+++ MongoApi/Program.cs
@@ -30,15 +30,15 @@ builder.Services.AddScoped<DatabaseService>(sp =>
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
